b8d3c548d319ed028b9b7814979e3a55a60a4239
Wesley Shann <sshann95@outlook.com>
2020-07-17 14:35:48 +0200

    config snapshot

--- android-runner-configuration/trepn/interaction.py
+++ android-runner-configuration/trepn/interaction.py
@@ -1,5 +1,14 @@
 # noinspection PyUnusedLocal
 def main(device, *args, **kwargs):
-    print('=INTERACTION=')
-    print((device.id))
-    print((device.current_activity()))
+    print('=INTERACTION_STARTasdadasdasdsa=')
+    # raise Exception("Sorry, no numbers below zero")
+    # run the experiment and check the stacktarce to see who calls who
+    # also try to run the monkeyreplay from here by creating a new MonkeyReplay(...).run(..)
+    #     you might need to create the interaction script directly inside android runner
+    # maybe move everything there?
+    print(("device.current_activity()", device.current_activity()))
+    print(("device", device), )
+    print(("device.id", device.id))
+    print(("args", args))
+    print(("kwargs", kwargs))
+    print('=INTERACTION_END=')
