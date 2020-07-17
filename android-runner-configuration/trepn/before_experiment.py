7ef75805d3d8fdd76eeb07a262358685f2dde7b0
Wesley Shann <sshann95@outlook.com>
2020-07-17 19:52:18 +0200

    Add script to clear the nappa database before the experiment
    Status - working

--- android-runner-configuration/trepn/before_experiment.py
+++ android-runner-configuration/trepn/before_experiment.py
@@ -1,3 +1,22 @@
+from AndroidRunner.pyand import ADB
+import time
+
+
 # noinspection PyUnusedLocal
 def main(device, *args, **kwargs):
+    #
+    adb = ADB(adb_path='/home/sshann/Android/Sdk/platform-tools/adb')
+    apps = [
+        'org.quantumbadger.redreader'
+    ]
+    dbs = [
+        'nappa.db',
+        'nappa.db-shm',
+        'nappa.db-wal',
+    ]
+    for app in apps:
+        for db in dbs:
+            result = adb.shell_command('run-as %s rm /data/data/%s/databases/%s' % (app, app, db))
+            result = result.decode('utf-8') if (isinstance(result, bytes) == True) else result
+            print("Deleting DB, app %s, file %s, result %s." % (app, db, result))
     pass
