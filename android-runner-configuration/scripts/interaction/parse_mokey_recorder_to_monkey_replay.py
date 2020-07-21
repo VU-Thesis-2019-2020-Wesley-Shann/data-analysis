b63133a6eaa56dce21956f4bb79c228fb5e8edab
Wesley Shann <sshann95@outlook.com>
2020-07-21 13:51:22 +0200

    Add interaction to UOB
    Status - Working

--- android-runner-configuration/scripts/interaction/parse_mokey_recorder_to_monkey_replay.py
+++ android-runner-configuration/scripts/interaction/parse_mokey_recorder_to_monkey_replay.py
@@ -1,9 +1,11 @@
 file_dir = '/home/sshann/Documents/thesis/experiments/android-runner-configuration/scripts/interaction/'
 file_name_list = [
-    'org.quantumbadger.redreader'
+    'org.quantumbadger.redreader',
+    'com.ak.uobtimetable'
 ]
 
-template_line = '{"type": "touch", "x": %s, "y": %s, "down": %s, "up": %s, "sleep": 4 }\n'
+template_line_touch = '{"type": "touch", "x": %s, "y": %s, "down": %s, "up": %s, "sleep": %s }\n'
+template_line_drag = '{"type": "drag", "points": [{"x": %s, "y": %s }, {"x": %s, "y": %s } ], "down": %s, "up": %s , "sleep": %s }\n'
 
 for file in file_name_list:
     src_file_path = file_dir + 'monkey_recorder/' + file
@@ -16,11 +18,22 @@ for file in file_name_list:
         cnt = 1
         while line:
             print("Line {}: {}".format(cnt, line.strip()))
-            x = line.split('\'x\':')[1].split(',')[0]
-            y = line.split('\'y\':')[1].split(',')[0]
-            print('y', y)
-            print('x', x)
-            parsed_line = template_line % (x, y, down_count, up_count)
+            if '\'sleep\'' in line:
+                sleep = line.split('\'sleep\':')[1].split(',')[0]
+            else:
+                sleep = '4'
+            if '\'drag\'' in line:
+                x1 = line.split('\'x1\':')[1].split(',')[0]
+                y1 = line.split('\'y1\':')[1].split(',')[0]
+                x2 = line.split('\'x2\':')[1].split(',')[0]
+                y2 = line.split('\'y2\':')[1].split(',')[0]
+                print(x1, y1, x2, y2, sleep)
+                parsed_line = template_line_drag % (x1, y1, x2, y2, down_count, up_count, sleep)
+            else:
+                x = line.split('\'x\':')[1].split(',')[0]
+                y = line.split('\'y\':')[1].split(',')[0]
+                print(x, y, sleep)
+                parsed_line = template_line_touch % (x, y, down_count, up_count, sleep)
             down_count += 2
             up_count += 2
             dst_file.write(parsed_line)
