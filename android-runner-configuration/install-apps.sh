8b62011ce49969a5c9a51aff48a8950c63cd89ac
Wesley Shann <sshann95@outlook.com>
2020-07-22 16:14:41 +0200

    Update bash scripts
    Status - Not tested, infile paths not updated

--- android-runner-configuration/install-apps.sh
+++ android-runner-configuration/install-apps.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh
 
 adb install /home/sshann/Documents/thesis/subjects/build/apks/baseline/org.quantumbadger.redreader.apk
+adb install /home/sshann/Documents/thesis/subjects/build/apks/baseline/io.github.project_travel_mate.apk
+adb install /home/sshann/Documents/thesis/subjects/build/apks/baseline/com.ak.uobtimetable.apk
+adb install /home/sshann/Documents/thesis/subjects/build/apks/baseline/com.newsblur.apk
+adb install /home/sshann/Documents/thesis/subjects/build/apks/baseline/de.danoeh.antennapod.debug.apk
+adb install /home/sshann/Documents/thesis/subjects/build/apks/baseline/io.github.hidroh.materialistic.apk
+adb install /home/sshann/Documents/thesis/subjects/build/apks/baseline/appteam.nith.hillffair.apk
--- android-runner-configuration/update_replay.sh
+++ android-runner-configuration/update_replay.sh
@@ -1,11 +1,4 @@
 #!/bin/zsh
 
-#python3 /home/sshann/Documents/thesis/experiments/android-runner-configuration/scripts/interaction/aggregate_mokey_files_to_parse.py
+python3 /home/sshann/Documents/thesis/experiments/android-runner-configuration/scripts/interaction/aggregate_mokey_files_to_parse.py
 python3 /home/sshann/Documents/thesis/experiments/android-runner-configuration/scripts/interaction/parse_mokey_recorder_to_monkey_replay.py
-
-#    "/home/sshann/Documents/thesis/subjects/build/apks/baseline/io.github.project_travel_mate.apk",
-#    "/home/sshann/Documents/thesis/subjects/build/apks/baseline/com.ak.uobtimetable.apk",
-#    "/home/sshann/Documents/thesis/subjects/build/apks/baseline/com.newsblur.apk",
-#    "/home/sshann/Documents/thesis/subjects/build/apks/baseline/de.danoeh.antennapod.debug.apk",
-#    "/home/sshann/Documents/thesis/subjects/build/apks/baseline/io.github.hidroh.materialistic.apk",
-#    "/home/sshann/Documents/thesis/subjects/build/apks/baseline/appteam.nith.hillffair.apk"
