1fdac17a65a6c5e5f7b9a116ff970a8ff39c1371
Wesley Shann <sshann95@outlook.com>
2020-07-22 22:59:56 +0200

    Use common interactions file in UOB
    Status - working

--- android-runner-configuration/scripts/interaction/python3/com.ak.uobtimetable.py
+++ android-runner-configuration/scripts/interaction/python3/com.ak.uobtimetable.py
@@ -1,19 +1,9 @@
-import time
+import sys
 
+sys.path.insert(0, '/home/sshann/Documents/thesis/experiments/android-runner-configuration/')
 
-def tap(device, x, y, sleep=4):
-    device.shell('input tap %s %s' % (x, y))
-    time.sleep(sleep)
-
-
-def swipe(device, x1, y1, x2, y2, sleep=4, duration=1000):
-    device.shell('input swipe %s %s %s %s %s' % (x1, y1, x2, y2, duration))
-    time.sleep(sleep)
-
-
-def tap_phone_back(device, sleep=1):
-    device.shell('input tap 324 2464')
-    time.sleep(sleep)
+from scripts.interaction.python3.common import tap
+from scripts.interaction.python3.common import swipe
 
 
 def tap_continue_button(device):
