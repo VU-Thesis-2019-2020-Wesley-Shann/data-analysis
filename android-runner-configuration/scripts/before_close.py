b95e04295177fe236226bab345683533b73d2b4b
Wesley Shann <sshann95@outlook.com>
2020-07-22 19:57:26 +0200

    Fix not removing cache file
    Status - working

--- android-runner-configuration/scripts/before_close.py
+++ android-runner-configuration/scripts/before_close.py
@@ -14,7 +14,7 @@ def main(device, *args, **kwargs):
     print('\tat"/data/data/%s/cache/"' % device.current_activity())
     result = device.shell(
         'run-as %s rm -rf /data/data/%s/cache/' % (device.current_activity(), device.current_activity()))
-    if device.current_activity().find('org.quantumbadger.redreader') == -1:
+    if device.current_activity().find('org.quantumbadger.redreader') != -1:
         clear_redreader_cache(device)
     print('result is', result)
     pass
