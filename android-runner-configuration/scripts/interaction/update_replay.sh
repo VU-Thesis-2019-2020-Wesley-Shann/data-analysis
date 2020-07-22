e3200b2c3be2369d262a41ac0f4791824a11ff51
Wesley Shann <sshann95@outlook.com>
2020-07-22 13:55:19 +0200

    Add absolute path and apks path
    Status - working

--- android-runner-configuration/scripts/interaction/update_replay.sh
+++ android-runner-configuration/scripts/interaction/update_replay.sh
@@ -1,4 +1,11 @@
 #!/bin/zsh
 
-python3 scripts/interaction/aggregate_mokey_files_to_parse.py
-python3 scripts/interaction/parse_mokey_recorder_to_monkey_replay.py
+python3 /home/sshann/Documents/thesis/experiments/android-runner-configuration/scripts/interaction/aggregate_mokey_files_to_parse.py
+python3 /home/sshann/Documents/thesis/experiments/android-runner-configuration/scripts/interaction/parse_mokey_recorder_to_monkey_replay.py
+
+#    "/home/sshann/Documents/thesis/subjects/build/apks/baseline/io.github.project_travel_mate.apk",
+#    "/home/sshann/Documents/thesis/subjects/build/apks/baseline/com.ak.uobtimetable.apk",
+#    "/home/sshann/Documents/thesis/subjects/build/apks/baseline/com.newsblur.apk",
+#    "/home/sshann/Documents/thesis/subjects/build/apks/baseline/de.danoeh.antennapod.debug.apk",
+#    "/home/sshann/Documents/thesis/subjects/build/apks/baseline/io.github.hidroh.materialistic.apk",
+#    "/home/sshann/Documents/thesis/subjects/build/apks/baseline/appteam.nith.hillffair.apk"
