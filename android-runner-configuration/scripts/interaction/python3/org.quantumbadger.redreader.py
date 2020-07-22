228156291251ca8af4e0597c6d3f2eedeeb81bed
Wesley Shann <sshann95@outlook.com>
2020-07-22 20:05:11 +0200

    Add new interaction to redreader
    Status - working

--- android-runner-configuration/scripts/interaction/python3/org.quantumbadger.redreader.py
+++ android-runner-configuration/scripts/interaction/python3/org.quantumbadger.redreader.py
@@ -73,9 +73,39 @@ def visit_subreddit_ask_science(device):
     tap_phone_back(device)
 
 
+def visit_subreddit_aww(device):
+    # 10 steps
+    print('Visit subReddit Aww')
+
+    # click in aww subreddit
+    tap(device, 600, 1733)
+
+    # click in post 1
+    tap(device, 600, 700)
+    tap_phone_back(device)
+
+    # click in post 2 comments
+    tap(device, 1340, 1100, 8)
+    tap_phone_back(device)
+
+    # Go back to home page
+    tap_phone_back(device)
+
+    # click in aww subreddit
+    tap(device, 600, 1733)
+
+    # click in post 2 comments
+    tap(device, 1340, 1100)
+    tap_phone_back(device)
+
+    # Go back to home page
+    tap_phone_back(device)
+
+
 # noinspection PyUnusedLocal
 def main(device, *args, **kwargs):
     if device.current_activity().find('org.quantumbadger.redreader') != -1:
         print('Running interaction for RedReader')
-        visit_subreddit_ask_science(device)
-        visit_subreddit_annoucements(device)
+        # visit_subreddit_ask_science(device)
+        # visit_subreddit_annoucements(device)
+        visit_subreddit_aww(device)
