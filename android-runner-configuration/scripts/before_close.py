44bbfc8866456ddb99ec463b84a00b82de361c19
Wesley Shann <sshann95@outlook.com>
2020-07-22 20:11:07 +0200

    Add spacing
    Status - working

--- android-runner-configuration/scripts/before_close.py
+++ android-runner-configuration/scripts/before_close.py
@@ -1,17 +1,17 @@
 def clear_redreader_cache(device):
     device.shell(
         'run-as %s rm /data/data/%s/databases/cache.db' % (device.current_activity(), device.current_activity()))
-    print('\tat"/data/data/%s/databases/cache.db"' % device.current_activity())
+    print('\tat "/data/data/%s/databases/cache.db"' % device.current_activity())
 
     device.shell('run-as %s rm /data/data/%s/databases/cache.db-journal' % (
         device.current_activity(), device.current_activity()))
-    print('\tat"/data/data/%s/databases/cache.db-journal"' % device.current_activity())
+    print('\tat "/data/data/%s/databases/cache.db-journal"' % device.current_activity())
 
 
 # noinspection PyUnusedLocal
 def main(device, *args, **kwargs):
     print('clearing app cache')
-    print('\tat"/data/data/%s/cache/"' % device.current_activity())
+    print('\tat "/data/data/%s/cache/"' % device.current_activity())
     result = device.shell(
         'run-as %s rm -rf /data/data/%s/cache/' % (device.current_activity(), device.current_activity()))
     if device.current_activity().find('org.quantumbadger.redreader') != -1:
