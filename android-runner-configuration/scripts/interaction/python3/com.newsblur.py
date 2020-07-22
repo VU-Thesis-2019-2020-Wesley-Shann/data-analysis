aaa014e283fc044ccb9554d70990e4835cbe3877
Wesley Shann <sshann95@outlook.com>
2020-07-22 23:10:19 +0200

    Add script files for remaining apps
    Status - not tested

---
+++ android-runner-configuration/scripts/interaction/python3/com.newsblur.py
@@ -0,0 +1,14 @@
+import sys
+
+sys.path.insert(0, '/home/sshann/Documents/thesis/experiments/android-runner-configuration/')
+
+from scripts.interaction.python3.common import tap
+from scripts.interaction.python3.common import tap_phone_back
+
+
+# noinspection PyUnusedLocal
+def main(device, *args, **kwargs):
+    if device.current_activity().find('com.newsblur') != -1:
+        print('Running interaction for NewsBlur')
+    else:
+        print('Skip file')
---
+++ android-runner-configuration/scripts/interaction/python3/io.github.hidroh.materialistic.py
@@ -0,0 +1,14 @@
+import sys
+
+sys.path.insert(0, '/home/sshann/Documents/thesis/experiments/android-runner-configuration/')
+
+from scripts.interaction.python3.common import tap
+from scripts.interaction.python3.common import tap_phone_back
+
+
+# noinspection PyUnusedLocal
+def main(device, *args, **kwargs):
+    if device.current_activity().find('io.github.hidroh.materialistic') != -1:
+        print('Running interaction for Materialistic')
+    else:
+        print('Skip file')
