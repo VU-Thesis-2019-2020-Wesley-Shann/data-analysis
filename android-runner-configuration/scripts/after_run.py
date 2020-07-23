59fef06cd6b38ab49c83009f453354b6e06dae8d
Wesley Shann <sshann95@outlook.com>
2020-07-23 12:23:15 +0200

    Close app after run
    Status - working but best to test with all

--- android-runner-configuration/scripts/after_run.py
+++ android-runner-configuration/scripts/after_run.py
@@ -1,6 +1,17 @@
+import time
+
+
+def close_app(device):
+    print('close app')
+    time.sleep(1)
+    device.shell('input tap 1134 2469')
+    time.sleep(2)
+    device.shell('input tap 1246 1360')
+    time.sleep(1)
+    device.shell('input keyevent KEYCODE_BACK')
+
+
 # noinspection PyUnusedLocal
 def main(device, *args, **kwargs):
-    # print("Run `input keyevent KEYCODE_WAKEUP`")
-    # result = device.shell('input keyevent KEYCODE_WAKEUP')
-    # print('display should be on', result)
+    close_app(device)
     pass
