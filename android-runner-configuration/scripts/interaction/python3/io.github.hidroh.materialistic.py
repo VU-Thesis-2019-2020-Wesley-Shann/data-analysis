51e39e278da3e3c0e18ccb2040b39b69c7f74127
Wesley Shann <sshann95@outlook.com>
2020-07-23 13:54:29 +0200

    Materialistic visit best stories page
    Status - Working

--- android-runner-configuration/scripts/interaction/python3/io.github.hidroh.materialistic.py
+++ android-runner-configuration/scripts/interaction/python3/io.github.hidroh.materialistic.py
@@ -52,10 +52,64 @@ def visit_catching_up(device):
     tap_phone_back(device)
 
 
+def visit_best_stories(device):
+    print('\tvisit_best_stories')
+
+    # open side menu
+    tap(device, 90, 192, 0)
+
+    # open more sections
+    tap(device, 427, 1322, 1)
+
+    # go to best stories
+    tap(device, 567, 1562)
+
+    # click on first card
+    tap(device, 724, 400, 8)
+
+    # click on comments
+    tap(device, 355, 693)
+
+    # return to feed list
+    tap_phone_back(device)
+
+    # click on second card
+    tap(device, 648, 789)
+
+    # click on comments
+    tap(device, 355, 693)
+
+    # return to feed list
+    tap_phone_back(device)
+
+    # return to front page
+    tap_phone_back(device)
+
+    # open side menu
+    tap(device, 90, 192, 0)
+
+    # this is kept open
+    # # open more sections
+    # tap(device, 427, 1322, 1)
+
+    # go to best stories
+    tap(device, 567, 1562)
+
+    # click on first card
+    tap(device, 724, 400)
+
+    # return to feed list
+    tap_phone_back(device)
+
+    # return to front page
+    tap_phone_back(device)
+
+
 # noinspection PyUnusedLocal
 def main(device, *args, **kwargs):
     if device.current_activity().find('io.github.hidroh.materialistic') != -1:
         print('\tRunning interaction for Materialistic')
-        visit_catching_up(device)
+        # visit_catching_up(device)
+        visit_best_stories(device)
     else:
         print('\tSkip file')
