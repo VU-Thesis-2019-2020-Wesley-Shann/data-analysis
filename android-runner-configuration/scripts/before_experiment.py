959e486f813e9c85e81aa0822ee505ac04d6fd8a
Wesley Shann <sshann95@outlook.com>
2020-07-28 14:19:48 +0200

    Move tasks into a function
    Status - should work

--- android-runner-configuration/scripts/before_experiment.py
+++ android-runner-configuration/scripts/before_experiment.py
@@ -7,9 +7,13 @@ from scripts.util.login import app_configuration
 from scripts.util.adb import install_apks
 
 
-# noinspection PyUnusedLocal
-def main(device, *args, **kwargs):
+def configure_apps(device):
     install_apks()
     app_configuration(device)
     clear_prefetch_db(device)
+
+
+# noinspection PyUnusedLocal
+def main(device, *args, **kwargs):
+    configure_apps(device)
     pass
