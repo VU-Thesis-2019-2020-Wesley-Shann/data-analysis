88c6a33f29b4e13ab5298fa22e7232cada130306
Wesley Shann <sshann95@outlook.com>
2020-07-29 21:25:22 +0200

    Rename metric

--- NappaExperimentation/app/src/main/java/nl/vu/cs/s2group/nappa/nappaexperimentation/MetricStrategyAccuracy.java
+++ NappaExperimentation/app/src/main/java/nl/vu/cs/s2group/nappa/nappaexperimentation/MetricStrategyAccuracy.java
@@ -1,10 +1,10 @@
 package nl.vu.cs.s2group.nappa.nappaexperimentation;
 
 @SuppressWarnings("unused")
-public class MetricStrategyHitMiss {
+public class MetricStrategyAccuracy {
     private static final String TAG = "MetricStrategyAccuracy";
 
-    private MetricStrategyHitMiss() {
+    private MetricStrategyAccuracy() {
         throw new IllegalStateException("MetricStrategyAccuracy is a metric class and should not be instantiated!");
     }
 
