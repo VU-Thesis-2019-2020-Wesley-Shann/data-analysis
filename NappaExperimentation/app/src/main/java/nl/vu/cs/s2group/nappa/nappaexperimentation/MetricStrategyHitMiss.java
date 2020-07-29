327c15c0d9ddf27fc42a99a58b750e391fa021ad
Wesley Shann <sshann95@outlook.com>
2020-07-29 21:24:45 +0200

    Log strategy hit miss metric

---
+++ NappaExperimentation/app/src/main/java/nl/vu/cs/s2group/nappa/nappaexperimentation/MetricStrategyHitMiss.java
@@ -0,0 +1,19 @@
+package nl.vu.cs.s2group.nappa.nappaexperimentation;
+
+@SuppressWarnings("unused")
+public class MetricStrategyHitMiss {
+    private static final String TAG = "MetricStrategyAccuracy";
+
+    private MetricStrategyHitMiss() {
+        throw new IllegalStateException("MetricStrategyAccuracy is a metric class and should not be instantiated!");
+    }
+
+    public static void log(int id, int hitCount, int missCount) {
+        double hitPercentage = (double) hitCount / ((double) hitCount + missCount);
+        String logMessage = "LOG_ID='" + id + "'" +
+                "HIT_PERCENTAGE='" + hitPercentage + "'," +
+                "HIT_COUNT='" + hitCount + "'," +
+                "MISS_COUNT='" + missCount + "',";
+        Logger.i(TAG, logMessage);
+    }
+}
