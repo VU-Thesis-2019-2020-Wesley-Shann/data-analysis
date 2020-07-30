cdf5326b819ec372a621244454af40e5f52c89e4
Wesley Shann <sshann95@outlook.com>
2020-07-30 12:29:35 +0200

    Log the number of children nodes

--- NappaExperimentation/app/src/main/java/nl/vu/cs/s2group/nappa/nappaexperimentation/MetricNappaPrefetchingStrategyExecutionTime.java
+++ NappaExperimentation/app/src/main/java/nl/vu/cs/s2group/nappa/nappaexperimentation/MetricNappaPrefetchingStrategyExecutionTime.java
@@ -8,11 +8,18 @@ public class MetricNappaPrefetchingStrategyExecutionTime {
         throw new IllegalStateException("MetricNappaPrefetchingStrategyExecutionTime is a metric class and should not be instantiated!");
     }
 
-    public static void log(String strategyClassSimpleName, long startedStrategyAtMillis, long completedStrategyAtMillis, int numberOfUrls) {
+    public static void log(String strategyClassSimpleName,
+                           long startedStrategyAtMillis,
+                           long completedStrategyAtMillis,
+                           int numberOfUrls,
+                           int numberOfChildrenNodes,
+                           int numberOfSelectedChildrenNodes) {
         long duration = completedStrategyAtMillis - startedStrategyAtMillis;
         String logMessage = "STRATEGY_CLASS='" + strategyClassSimpleName + "'" +
                 "DURATION='" + duration + "'," +
-                "NUMBER_OF_URLS='" + numberOfUrls + "',";
+                "NUMBER_OF_URLS='" + numberOfUrls + "'," +
+                "NUMBER_OF_SELECTED_CHILDREN_NODES='" + numberOfSelectedChildrenNodes + "'," +
+                "NUMBER_OF_CHILDREN_NODES='" + numberOfChildrenNodes + "',";
         Logger.i(TAG, logMessage);
     }
 }
