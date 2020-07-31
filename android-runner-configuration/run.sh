ea8aac58587b1289092229564e9ac3c92e65191a
Wesley Shann <sshann95@outlook.com>
2020-07-31 13:44:48 +0200

    Display time (and other resources) from running AR
    Status - to be tested

--- android-runner-configuration/run.sh
+++ android-runner-configuration/run.sh
@@ -1,1 +1,1 @@
-sudo python3 /home/sshann/Documents/thesis/android-runner /home/sshann/Documents/thesis/experiments/android-runner-configuration/nexus6p_trepn.json
+time sudo python3 /home/sshann/Documents/thesis/android-runner /home/sshann/Documents/thesis/experiments/android-runner-configuration/nexus6p_trepn.json
