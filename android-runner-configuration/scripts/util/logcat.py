c333d6e92adb55939f1492691b6dd6b7cb15fa32
Wesley Shann <sshann95@outlook.com>
2020-08-01 16:20:33 +0200

    Add logcat to save
    Status - to be tested

--- android-runner-configuration/scripts/util/logcat.py
+++ android-runner-configuration/scripts/util/logcat.py
@@ -77,6 +77,13 @@ def save_raw_log_cat_to_file(device):
             'filter_per_tag': True,
         },
         {
+            'tag': 'NappaLifecycleObserver',
+            'base_path': base_path + 'nappalifecycle/',
+            'file_name': file_name,
+            'should_save': device.current_activity().find('nappa') != -1,
+            'filter_per_tag': True,
+        },
+        {
             'tag': '',
             'base_path': base_path + 'full-logcat/',
             'file_name': file_name,
