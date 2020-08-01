ec7590644f7ac7aa67e445fbe81e721bc1672afa
Wesley Shann <sshann95@outlook.com>
2020-08-01 19:43:01 +0200

    Take screenshot before close
    Status - to be tested

--- android-runner-configuration/scripts/before_close.py
+++ android-runner-configuration/scripts/before_close.py
@@ -1,4 +1,9 @@
 import sys
+import os
+import subprocess
+
+# noinspection PyUnresolvedReferences
+from paths import paths_dict
 
 sys.path.insert(0, '/home/sshann/Documents/thesis/experiments/android-runner-configuration/')
 
@@ -6,6 +11,7 @@ from scripts.util.file import clear_dir
 from scripts.util.file import clear_db
 from scripts.util.adb import copy_nappa_db_to_sd_card
 from scripts.util.logcat import retrieve_logcat_info
+from scripts.util.logcat import get_formatted_timestamp
 
 
 def clear_app_data(device):
@@ -15,8 +21,17 @@ def clear_app_data(device):
     copy_nappa_db_to_sd_card(device)
 
 
+def take_screenshot():
+    file_name = '%s.png' % get_formatted_timestamp()
+    base_pah = os.path.join(paths_dict()['OUTPUT_DIR'], 'screenshot')
+    path = os.path.join(base_pah, file_name)
+    command = 'adb exec-out screencap -p > %s' % path
+    subprocess.call(command, shell=True)
+
+
 # noinspection PyUnusedLocal
 def main(device, *args, **kwargs):
     clear_app_data(device)
     retrieve_logcat_info(device)
+    take_screenshot()
     pass
