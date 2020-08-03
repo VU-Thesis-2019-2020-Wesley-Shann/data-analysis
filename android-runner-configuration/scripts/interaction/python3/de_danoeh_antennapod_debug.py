4919afd8f376ee5558e8632aa7b2fc16d3c3d7d3
Wesley Shann <sshann95@outlook.com>
2020-08-03 12:35:03 +0200

    Add interaction to ANtenna Pod
    Status - working

--- android-runner-configuration/scripts/interaction/python3/de_danoeh_antennapod_debug.py
+++ android-runner-configuration/scripts/interaction/python3/de_danoeh_antennapod_debug.py
@@ -11,7 +11,6 @@ def configure_settings(device):
     print('\tconfigure_settings')
 
     # open side menu
-    tap(device, 1233, 1274, 1)
     tap(device, 103, 192, 0)
 
     # Click settings
@@ -33,28 +32,35 @@ def configure_settings(device):
     tap_phone_back(device)
 
 
-def visit_subscriptions_and_podcasts(device):
-    print('\tvisit_subscriptions')
+def visit_add_podcast_itunes(device):
+    print('\tvisit_add_podcast_itunes')
 
     # open side menu
     # tap(device, 1233, 1274, 1)
     tap(device, 103, 192, 1)
 
-    # click in subscriptions
-    tap(device, 427, 522, 1)
+    # # click in subscriptions
+    # tap(device, 427, 522, 1)
 
-    # click add podcast
-    tap(device, 225, 549, 1)
+    # # click add podcast from subscriptions
+    # tap(device, 225, 549, 1)
+
+    # click add podcast from side menu
+    tap(device, 409, 1034, 1)
 
     # click search itunes
     tap(device, 567, 714, 6)
 
     # click card 1
-    tap(device, 616, 469, 8)
+    tap(device, 616, 469)
+    # Subscribe to podcast 1
+    tap(device, 625, 832)
     tap_phone_back(device)
 
     # click card 2
     tap(device, 639, 762)
+    # Subscribe to podcast 2
+    tap(device, 693, 821)
     tap_phone_back(device)
 
     # click card 2
@@ -78,6 +84,38 @@ def visit_subscriptions_and_podcasts(device):
     # back to add podcast
     tap_phone_back(device, 1)
 
+
+def visit_podcast(device):
+    print('\tvisit_podcast')
+
+    # open side menu
+    tap(device, 103, 192, 1)
+
+    # click in podcast from side menu
+    tap(device, 625, 1248)
+
+    # click in podcast configuration
+    tap(device, 1363, 517)
+    tap_phone_back(device)
+
+    # click in podcast info
+    tap(device, 1372, 384)
+    tap_phone_back(device)
+
+    # click in podcast item
+    tap(device, 733, 896)
+    tap_phone_back(device)
+
+
+def visit_add_podcast_gpodder(device):
+    print('\tvisit_add_podcast_gpodder')
+
+    # open side menu
+    tap(device, 103, 192, 1)
+
+    # click add podcast from side menu
+    tap(device, 409, 1034, 1)
+
     # click on browse gpodder
     tap(device, 603, 1061, 6)
 
@@ -100,9 +138,6 @@ def visit_subscriptions_and_podcasts(device):
     tap_phone_back(device)
 
     # back to add podcast
-    tap_phone_back(device)
-
-    # back subscriptions
     tap_phone_back(device)
 
 
@@ -117,5 +152,8 @@ def main(device, *args, **kwargs):
 
 def run_antenna_pod_interaction(device):
     print('\tRunning interaction for AntennaPod')
+    tap(device, 1233, 1274, 1)
     configure_settings(device)
-    visit_subscriptions_and_podcasts(device)
+    visit_add_podcast_itunes(device)
+    visit_podcast(device)
+    visit_add_podcast_gpodder(device)
