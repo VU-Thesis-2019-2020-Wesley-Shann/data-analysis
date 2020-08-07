b95211bae987d5e08cad178d6cac7b04c3050e70
Wesley Shann <sshann95@outlook.com>
2020-08-07 18:34:43 +0200

    Remove global summary

--- data-analysis/rq1.R
+++ data-analysis/rq1.R
@@ -18,21 +18,6 @@ source("util/plot.R")
 print("Reading data")
 rq1.dataframe <- experiment.source.runtime()
 
-# Take summary from the data and write to file
-print("Summarizing data")
-rq1.columns_to_take_summary <- c("run.duration.s", "android.memory.mb", "trepn.cpu")
-rq1.filter.non_zero_battery <- rq1.dataframe$trepn.battery.joule != 0
-rq1.summary <- cbind(
-  as.matrix(summary(rq1.dataframe[rq1.filter.non_zero_battery,]$trepn.battery.nonzero.joule)),
-  summary(rq1.dataframe[rq1.columns_to_take_summary])
-)
-colnames(rq1.summary)[1] <- "trepn.battery.nonzero.joule"
-experiment.write.latex(rq = 1,
-                       dataframe = t(rq1.summary),
-                       file_name = "data-summary.tex",
-                       caption = "Overview of the runtime metrics per subject and treatment.",
-                       label = "tab:results:rq1:summary:all")
-
 # Take the duration time per subject and write to file
 print("Summarizing runtime duration")
 rq1.summary.run.duration <- experiment.subject.summary(dataframe = rq1.dataframe, property = "run.duration.s")
