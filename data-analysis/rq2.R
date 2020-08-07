90018f106caeae768977f3c6e76926e8d79e0355
Wesley Shann <sshann95@outlook.com>
2020-08-07 15:00:27 +0200

    Plot some charts

--- data-analysis/rq2.R
+++ data-analysis/rq2.R
@@ -11,9 +11,10 @@ source("util/read_data.R")
 rq2.dataframe <- experiment.source.network_request_execution_time()
 
 aes <- aes_string(
-  #y = "request.duration.from_system.ms",
-  y = "response.length.from_okhttp.b",
-  x = "subject.id"
+#y = "request.duration.from_system.ms",
+y = "response.length.from_okhttp.b",
+#y = "response.length.from_header.b",
+x = "subject.id"
 )
 
 
@@ -25,7 +26,12 @@ labs <- labs(
 
 )
 
-ggplot(rq2.dataframe, aes) +
+rq2.index.has_length_from_okhttp <- rq2.dataframe$response.length.from_okhttp.b >= 0
+rq2.index.has_length_from_header <- rq2.dataframe$response.length.from_header.b >= 0
+rq2.index.length_from_okhttp_is_below_1_MB <- rq2.dataframe$response.length.from_okhttp.b <= 10^6
+rq2.index.length_from_okhttp_is_below_100_KB <- rq2.dataframe$response.length.from_okhttp.b <= 100* 10^3
+
+ggplot(rq2.dataframe[rq2.index.has_length_from_okhttp & rq2.index.length_from_okhttp_is_below_100_KB ,], aes) +
   geom_boxplot() +
   labs +
   theme
