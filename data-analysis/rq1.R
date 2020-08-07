c755441836af9bc71ba0d9290c94964727a9b3e6
Wesley Shann <sshann95@outlook.com>
2020-08-07 20:22:43 +0200

    Make CPU load plots

--- data-analysis/rq1.R
+++ data-analysis/rq1.R
@@ -65,13 +65,27 @@ experiment.write.latex(rq = 1,
 print("Generating plots")
 
 my_plot <- experiment.plot.boxplot.subject(rq1.dataframe[rq1.filter.non_zero_battery,],
-                        "trepn.battery.nonzero.joule",
-                        "Battery comsumption (J)",
-                        "Battery comsumption per subject")
+                                           "trepn.battery.nonzero.joule",
+                                           "Battery comsumption (J)",
+                                           "Battery comsumption per subject")
 experiment.write.plot(filename = "boxplot_battery_per_subject_treatment.png", rq = 1)
 
-my_plot <-experiment.plot.violin.treatment(rq1.dataframe[rq1.filter.non_zero_battery,],
-                        "trepn.battery.nonzero.joule",
-                        "Battery comsumption (J)",
-                        "Battery comsumption per treatment")
+
+my_plot <- experiment.plot.violin.treatment(rq1.dataframe[rq1.filter.non_zero_battery,],
+                                            "trepn.battery.nonzero.joule",
+                                            "Battery comsumption (J)",
+                                            "Battery comsumption per treatment")
 experiment.write.plot(filename = "violin_battery_per_treatment.png", rq = 1)
+
+my_plot <- experiment.plot.boxplot.subject(rq1.dataframe,
+                                           "trepn.cpu",
+                                           "CPU load (%)",
+                                           "CPU load per subject")
+experiment.write.plot(filename = "boxplot_cpu_per_subject_treatment.png", rq = 1)
+
+
+my_plot <- experiment.plot.violin.treatment(rq1.dataframe,
+                                            "trepn.cpu",
+                                            "CPU load (%)",
+                                            "CPU load per treatment")
+experiment.write.plot(filename = "violin_cpu_per_treatment.png", rq = 1)
