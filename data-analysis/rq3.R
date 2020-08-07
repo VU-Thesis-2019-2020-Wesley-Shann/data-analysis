f5e869b998921bc00fdc641d5e434ec7d0e7eb2d
Wesley Shann <sshann95@outlook.com>
2020-08-07 12:48:00 +0200

    Fix typo

--- data-analysis/rq3.R
+++ data-analysis/rq3.R
@@ -18,7 +18,7 @@ theme <- theme(
   axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)
 )
 
-rq3.subjects.abobe.0 <- c("Antenna Pod", "Hill'Fair", "Materialistic")
-ggplot(rq3.dataframe[rq3.dataframe$subject.name %in% rq3.subjects.abobe.0,] , aes) +
+rq3.subjects.above.0 <- c("Antenna Pod", "Hill'Fair", "Materialistic")
+ggplot(rq3.dataframe[rq3.dataframe$subject.name %in% rq3.subjects.above.0,] , aes) +
   geom_boxplot() +
   theme
