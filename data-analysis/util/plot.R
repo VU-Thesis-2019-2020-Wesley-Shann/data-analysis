98dd2ebd4ea19b8b5760fe1516122865a70712f5
Wesley Shann <sshann95@outlook.com>
2020-08-07 19:38:43 +0200

    Add colour per subject to plots

--- data-analysis/util/plot.R
+++ data-analysis/util/plot.R
@@ -6,7 +6,8 @@
 experiment.plot.boxplot <- function(dataframe, axis_y_column, axis_y_legend, title) {
   aes <- aes_string(
     x = "subject.id",
-    y = axis_y_column
+    y = axis_y_column,
+    fill = "subject.name"
   )
 
   labs <- labs(
@@ -16,7 +17,8 @@ experiment.plot.boxplot <- function(dataframe, axis_y_column, axis_y_legend, tit
   )
 
   theme <- theme(
-    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+    legend.position = "none"
   )
 
   ggplot(dataframe, aes) +
