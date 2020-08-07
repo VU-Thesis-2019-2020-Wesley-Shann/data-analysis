801a34c2fc0fa1015e76a69c3b986ccc81db5061
Wesley Shann <sshann95@outlook.com>
2020-08-07 12:47:39 +0200

    Verify accuracy in plot

--- data-analysis/rq3.R
+++ data-analysis/rq3.R
@@ -8,3 +8,17 @@ source("util/read_data.R")
 
 rq3.dataframe <- experiment.source.prefetching.accuracy()
 
+aes <- aes_string(
+  y = "f1.score",
+  x = "subject.id.short"
+)
+
+
+theme <- theme(
+  axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)
+)
+
+rq3.subjects.abobe.0 <- c("Antenna Pod", "Hill'Fair", "Materialistic")
+ggplot(rq3.dataframe[rq3.dataframe$subject.name %in% rq3.subjects.abobe.0,] , aes) +
+  geom_boxplot() +
+  theme
