c72ac0e3a5026f62041b21ae7332ef7b87437599
Wesley Shann <sshann95@outlook.com>
2020-08-12 12:12:44 +0200

    Clean up script and output

--- data-analysis/rq2.R
+++ data-analysis/rq2.R
@@ -73,44 +73,12 @@ experiment.write.latex(rq = 2,
                        dataframe = rq2.summary.subject.datapoints,
                        filename = "summary-treatment_subject_datapoints.tex")
 
-
-subject <- "Materialistic (G)"
-#for (subject in levels(rq2.dataframe$subject.id)) {
-  for (part in levels(rq2.dataframe$experiment.part)) {
-    for (run in levels(rq2.dataframe$run.number)) {
-      datapoints <- rq2.dataframe[rq2.dataframe$subject.id == subject &
-                                    rq2.dataframe$experiment.part == part &
-                                    rq2.dataframe$run.number == run,]
-      print(paste(subject, part, run, nrow(datapoints), length(unique(datapoints$request.url))))
-    }
-  }
-#}
-#rq2.summary.subject.url <- data.frame(matrix(ncol = 3, nrow = 0))
-#for (subject in levels(rq2.dataframe$subject.name)) {
-#  rq2.summary.subject.url <- rbind(
-#    rq2.summary.subject.url,
-#    c(subject,
-#      length(unique(rq2.dataframe[rq2.dataframe$subject.name == subject,]$request.url)),
-#      length(rq2.dataframe[rq2.dataframe$subject.name == subject,]$request.url)
-#    )
-#  )
-#}
-#colnames(rq2.summary.subject.url) <- c("Subject", "Unique URLs", "Total URLs")
-#experiment.write.latex(dataframe = rq2.summary.subject.url,
-#                       rq = 2,
-#                       filename = "summary_subject_url",
-#                       caption = "Count of unique requested URL per subject",
-#                       label = "tab:results:rq2:summary:subject:url")
-
-
 # Per treatment
 print("Descriptive statistics per treatment")
 rq2.summary.treatment.request_duration <- experiment.treatment.summary(dataframe = rq2.dataframe, property = "request.duration.from_system.ms", digits = 2)
 experiment.write.latex(rq = 2,
                        dataframe = t(rq2.summary.treatment.request_duration),
                        filename = "summary-treatment_request_duration.tex")
-#rq2.summary.treatment.request_duration <- rq2.summary.treatment.request_duration[-kepp_min_max_mean_median,]
-rownames(rq2.summary.treatment.request_duration) <- paste("Request duration (ms)", rownames(rq2.summary.treatment.request_duration))
 
 # without materialistic
 rq2.summary.treatment.request_duration_without_materialistic <- experiment.treatment.summary(dataframe = rq2.dataframe[rq2.filter.no_materialistic,], property = "request.duration.from_system.ms", digits = 2)
@@ -122,20 +90,6 @@ rq2.summary.treatment.response_length <- experiment.treatment.summary(dataframe 
 experiment.write.latex(rq = 2,
                        dataframe = t(rq2.summary.treatment.response_length),
                        filename = "summary-treatment_request_length.tex")
-#rq2.summary.treatment.response_length <- rq2.summary.treatment.response_length[-kepp_min_max_mean_median,]
-rownames(rq2.summary.treatment.response_length) <- paste("Response length (KB)", rownames(rq2.summary.treatment.response_length))
-
-rq2.summary.treatment.aggregate <- rbind(
-  rq2.summary.treatment.request_duration,
-  rq2.summary.treatment.response_length
-)
-rq2.summary.treatment.aggregate
-experiment.write.latex(rq = 2,
-                       digits = 2,
-                       dataframe = rq2.summary.treatment.aggregate,
-                       filename = "summary_treatment.tex",
-                       caption = "Overview of the user perceived latency per treatment.",
-                       label = "tab:results:rq2:summary:treatment")
 
 rq2.summary.treatment.url <- data.frame(matrix(ncol = 3, nrow = 0))
 for (treatment in levels(rq2.dataframe$subject.treatment.name.long)) {
@@ -173,25 +127,6 @@ experiment.write.latex(rq = 2,
 #rq2.summary.subject.response_length <- rq2.summary.subject.response_length[-kepp_min_max_mean_median,]
 rownames(rq2.summary.subject.response_length) <- paste("Response length (KB)", rownames(rq2.summary.subject.response_length))
 
-# Treatment ~ subject
-
-rq2.summary.treatment_subject.url <- data.frame(matrix(ncol = 3, nrow = 0))
-for (subject in levels(rq2.dataframe$subject.id)) {
-  rq2.summary.treatment_subject.url <- rbind(
-    rq2.summary.treatment_subject.url,
-    c(subject,
-      length(unique(rq2.dataframe[rq2.dataframe$subject.id == subject,]$request.url)),
-      length(rq2.dataframe[rq2.dataframe$subject.id == subject,]$request.url)
-    )
-  )
-}
-colnames(rq2.summary.treatment_subject.url) <- c("Subject", "Unique URLs", "Total URLs")
-experiment.write.latex(dataframe = rq2.summary.treatment_subject.url,
-                       rq = 2,
-                       filename = "summary_treatment_subject_url",
-                       caption = "Count of unique requested URL per subject and treatment",
-                       label = "tab:results:rq2:summary:treatment_subject:url")
-
 #######################################  Phase 1b Plots ########################################
 print("Generating plots")
 
@@ -214,12 +149,6 @@ my_plot <- experiment.plot.boxplot(rq2.dataframe[rq2.filter.no_uob,],
                                    "Response length (Without UOB)")
 experiment.write.plot(filename = "boxplot_response_length_no_uob.png", rq = 2)
 
-my_plot <- experiment.plot.boxplot(rq2.dataframe[rq2.filter.response.length.below_1000kb,],
-                                   "response.length.from_okhttp.kb",
-                                   "Response length (KB)",
-                                   "Response length (Below 1MB)")
-experiment.write.plot(filename = "boxplot_response_length_below_1000kb.png", rq = 2)
-
 # Violin plot
 my_plot <- experiment.plot.violin(rq2.dataframe,
                                   "request.duration.from_system.ms",
@@ -233,18 +162,6 @@ my_plot <- experiment.plot.violin(rq2.dataframe[rq2.filter.no_materialistic,],
                                   "Request duration (Without Materialistic)")
 experiment.write.plot(filename = "violin_request_duration_no_materialistic.png", rq = 2)
 
-my_plot <- experiment.plot.violin(rq2.dataframe[rq2.filter.with_prefetch_only,],
-                                  "request.duration.from_system.ms",
-                                  "Request duration (ms)",
-                                  "Request duration (Prefetching F1-Score>0)")
-experiment.write.plot(filename = "violin_request_duration_with_prefetch_only.png", rq = 2)
-
-my_plot <- experiment.plot.violin(rq2.dataframe[rq2.filter.with_prefetch_only & rq2.filter.no_materialistic,],
-                                  "request.duration.from_system.ms",
-                                  "Request duration (ms)",
-                                  "Request duration (Prefetching F1>0; No Materislistic)")
-experiment.write.plot(filename = "violin_request_duration_with_prefetch_only_no_materialistic.png", rq = 2)
-
 # Frequency
 my_plot <- experiment.plot.freqpoly(rq2.dataframe,
                                     "request.duration.from_system.ms",
@@ -257,18 +174,6 @@ my_plot <- experiment.plot.freqpoly(rq2.dataframe[rq2.filter.no_materialistic,],
                                     "Request duration (ms)",
                                     "Request duration (Without Materialistic)")
 experiment.write.plot(filename = "freqpoly_request_duration_no_materialistic.png", rq = 2)
-
-my_plot <- experiment.plot.freqpoly(rq2.dataframe[rq2.filter.with_prefetch_only,],
-                                    "request.duration.from_system.ms",
-                                    "Request duration (ms)",
-                                    "Request duration (Prefetching F1-Score>0)")
-experiment.write.plot(filename = "freqpoly_request_duration_with_prefetch_only.png", rq = 2)
-
-my_plot <- experiment.plot.freqpoly(rq2.dataframe[rq2.filter.with_prefetch_only & rq2.filter.no_materialistic,],
-                                    "request.duration.from_system.ms",
-                                    "Request duration (ms)",
-                                    "Request duration (Prefetching F1>0; No Materislistic)")
-experiment.write.plot(filename = "freqpoly_request_duration_with_prefetch_only_no_materialistic.png", rq = 2)
 
 
 #################################################################################################
